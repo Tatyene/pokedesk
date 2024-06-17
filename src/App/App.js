689dc4b1b91dea5561a4775a3a517f44ac97d9d2
Tatyene Alves <tatyene.alves@scania.com>
2024-06-17 15:11:36 -0300

    feat: gerado header

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,13 @@
 import './App.css';
+import imagem from '../image/Pokedex.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>
-          Learn React
-        </a>
+        <div class="image-header">
+          <img src={imagem} alt="Pokedex" />
+        </div>
       </header>
     </div>
   );
